be3fb2e093b20fcf3c2015a80abb87e8eb04b97e
athyzz <atheenaadmahara@gmail.com>
2025-07-02 14:43:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,17 +11,22 @@ const stickers = [
   'assets/star.png',
   'assets/mic.png',
 ];
+
+// Load stickers
 stickers.forEach(src => {
   const img = document.createElement('img');
   img.src = src;
+  img.style.width = '80px';
+  img.style.margin = '10px';
+  img.style.cursor = 'pointer';
   img.onclick = () => {
     const clone = img.cloneNode();
-    clone.style.top = '20px';
-    clone.style.left = '20px';
     clone.style.position = 'absolute';
+    clone.style.top = '100px';
+    clone.style.left = '100px';
     clone.draggable = true;
     stickerContainer.appendChild(clone);
-    clone.onmousedown = drag;
+    enableDrag(clone);
   };
   stickerContainer.appendChild(img);
 });
@@ -32,15 +37,10 @@ startBtn.onclick = () => {
     .then(stream => {
       video.srcObject = stream;
     })
-  startBtn.onclick = () => {
-  cameraSection.classList.remove('hidden');
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => { video.srcObject = stream; })
     .catch(err => {
       console.error(err);
-      alert('Kamera tidak bisa diakses. Pastikan kamu sudah mengizinkan akses dan browser kamu mendukung fitur ini.');
+      alert('Kamera tidak bisa diakses. Pastikan kamu sudah izinkan akses kamera ya sayy 😢');
     });
-};
 };
 
 captureBtn.onclick = () => {
@@ -48,12 +48,17 @@ captureBtn.onclick = () => {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   ctx.drawImage(video, 0, 0);
-  Array.from(stickerContainer.children).forEach(el => {
-    if (el.tagName === 'IMG') {
-      const rect = el.getBoundingClientRect();
-      ctx.drawImage(el, el.offsetLeft, el.offsetTop, el.width, el.height);
-    }
+
+  // Tempel stiker ke canvas
+  const stickers = stickerContainer.querySelectorAll('img');
+  stickers.forEach(sticker => {
+    const rect = sticker.getBoundingClientRect();
+    const videoRect = video.getBoundingClientRect();
+    const x = rect.left - videoRect.left;
+    const y = rect.top - videoRect.top;
+    ctx.drawImage(sticker, x, y, sticker.width, sticker.height);
   });
+
   downloadBtn.classList.remove('hidden');
 };
 
@@ -64,23 +69,27 @@ downloadBtn.onclick = () => {
   link.click();
 };
 
-function drag(e) {
-  const el = e.target;
-  const shiftX = e.clientX - el.getBoundingClientRect().left;
-  const shiftY = e.clientY - el.getBoundingClientRect().top;
+// Drag function
+function enableDrag(el) {
+  el.onmousedown = function (e) {
+    e.preventDefault();
+    let shiftX = e.clientX - el.getBoundingClientRect().left;
+    let shiftY = e.clientY - el.getBoundingClientRect().top;
 
-  function moveAt(pageX, pageY) {
-    el.style.left = pageX - shiftX + 'px';
-    el.style.top = pageY - shiftY + 'px';
-  }
+    function moveAt(pageX, pageY) {
+      el.style.left = pageX - shiftX + 'px';
+      el.style.top = pageY - shiftY + 'px';
+    }
 
-  function onMouseMove(e) {
-    moveAt(e.pageX, e.pageY);
-  }
+    function onMouseMove(e) {
+      moveAt(e.pageX, e.pageY);
+    }
 
-  document.addEventListener('mousemove', onMouseMove);
-  el.onmouseup = function () {
-    document.removeEventListener('mousemove', onMouseMove);
-    el.onmouseup = null;
+    document.addEventListener('mousemove', onMouseMove);
+    el.onmouseup = function () {
+      document.removeEventListener('mousemove', onMouseMove);
+      el.onmouseup = null;
+    };
   };
+  el.ondragstart = () => false;
 }
